1da6c1a98f17667ca8c9e69ca2b45c31a562b111
Thomas ten Cate <ttencate@gmail.com>
2020-12-03 12:24:08 +0100

    Solve 03a and 03b

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,21 +1,51 @@
-fn part1(_input: &str) -> String {
-    "TODO".to_string()
+type Cell = u8;
+type Grid = Vec<Vec<Cell>>;
+const TREE: u8 = b'#';
+
+fn parse(input: &str) -> Grid {
+    input
+        .lines()
+        .map(|line| line.bytes().collect::<Vec<_>>())
+        .collect::<Vec<_>>()
+}
+
+fn count_trees(grid: &Grid, x_step: usize, y_step: usize) -> usize {
+    let mut x = x_step;
+    let mut y = y_step;
+    let mut count = 0;
+    while y < grid.len() {
+        let line = &grid[y];
+        if line[x % line.len()] == TREE {
+            count += 1;
+        }
+        x += x_step;
+        y += y_step;
+    }
+    count
+}
+
+fn part1(input: &str) -> usize {
+    let grid = parse(input);
+    count_trees(&grid, 3, 1)
 }
 
 #[test]
 fn test_part1() {
-    // assert_eq!(part1(&aoc::example(0)), );
-    // assert_eq!(part1(&aoc::input()), );
+    assert_eq!(part1(&aoc::example(0)), 7);
+    assert_eq!(part1(&aoc::input()), 191);
 }
 
-fn part2(_input: &str) -> String {
-    "TODO".to_string()
+fn part2(input: &str) -> usize {
+    let grid = parse(input);
+    [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)].iter()
+        .map(|&(x_step, y_step)| count_trees(&grid, x_step, y_step))
+        .product()
 }
 
 #[test]
 fn test_part2() {
-    // assert_eq!(part2(&aoc::example(0)), );
-    // assert_eq!(part2(&aoc::input()), );
+    assert_eq!(part2(&aoc::example(0)), 336);
+    assert_eq!(part2(&aoc::input()), 1478615040);
 }
 
 fn main() {
