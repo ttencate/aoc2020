22645c17474e1fe61b9be918273a11ce0bb64882
Thomas ten Cate <ttencate@gmail.com>
2020-12-06 09:00:20 +0100

    Solve 06a and 06b

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,21 +1,47 @@
-fn part1(_input: &str) -> String {
-    "TODO".to_string()
+#![feature(iterator_fold_self)]
+
+use std::collections::HashSet;
+
+fn part1(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|group| {
+            group
+                .chars()
+                .filter(|&c| 'a' <= c && c <= 'z')
+                .collect::<HashSet<_>>()
+                .len()
+        })
+        .sum()
 }
 
 #[test]
 fn test_part1() {
-    // assert_eq!(part1(&aoc::example(0)), );
-    // assert_eq!(part1(&aoc::input()), );
+    assert_eq!(part1(&aoc::example(0)), 6);
+    assert_eq!(part1(&aoc::example(1)), 11);
+    assert_eq!(part1(&aoc::input()), 6596);
 }
 
-fn part2(_input: &str) -> String {
-    "TODO".to_string()
+fn part2(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|group| {
+            group
+                .trim_end()
+                .split("\n")
+                .map(|s| s.chars().collect::<HashSet<_>>())
+                .fold_first(|a, b| a.intersection(&b).copied().collect())
+                .unwrap()
+                .len()
+        })
+        .sum()
 }
 
 #[test]
 fn test_part2() {
-    // assert_eq!(part2(&aoc::example(0)), );
-    // assert_eq!(part2(&aoc::input()), );
+    assert_eq!(part2(&aoc::example(0)), 3);
+    assert_eq!(part2(&aoc::example(1)), 6);
+    assert_eq!(part2(&aoc::input()), 3219);
 }
 
 fn main() {
