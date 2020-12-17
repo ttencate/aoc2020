9cf1dc71365435a41dddee2cf2e933ab00b8094d
Thomas ten Cate <ttencate@gmail.com>
2020-12-17 11:55:59 +0100

    Use SIMD for a moderate speedup (58 ms -> 38 ms)

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,69 +1,41 @@
+use packed_simd::SimdVector;
 use std::collections::{HashMap, HashSet};
-use std::hash::Hash;
-use std::ops::Add;
 
-trait Coord: Copy + std::hash::Hash + Eq + Sized + Add<Output = Self> {
-    fn neighbors() -> Vec<Self>;
+type Coord = packed_simd::i32x4;
+
+fn neighbors_3d() -> Vec<Coord> {
+    let mut result = Vec::with_capacity(26);
+    for dz in -1..2 {
+        for dy in -1..2 {
+            for dx in -1..2 {
+                if dx != 0 || dy != 0 || dz != 0 {
+                    result.push(Coord::new(dx, dy, dz, 0));
+                }
+            }
+        }
+    }
+    result
 }
 
-#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Hash)]
-struct Coord3(i64, i64, i64);
-
-impl Coord for Coord3 {
-    fn neighbors() -> Vec<Self> {
-        let mut result = Vec::with_capacity(26);
+fn neighbors_4d() -> Vec<Coord> {
+    let mut result = Vec::with_capacity(80);
+    for dw in -1..2 {
         for dz in -1..2 {
             for dy in -1..2 {
                 for dx in -1..2 {
-                    if dx != 0 || dy != 0 || dz != 0 {
-                        result.push(Coord3(dx, dy, dz));
+                    if dx != 0 || dy != 0 || dz != 0 || dw != 0 {
+                        result.push(Coord::new(dx, dy, dz, dw));
                     }
                 }
             }
         }
-        result
     }
+    result
 }
 
-impl Add for Coord3 {
-    type Output = Coord3;
-    fn add(self, other: Coord3) -> Coord3 {
-        Coord3(self.0 + other.0, self.1 + other.1, self.2 + other.2)
-    }
-}
+type State = HashSet<Coord>;
 
-#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Hash)]
-struct Coord4(i64, i64, i64, i64);
-
-impl Coord for Coord4 {
-    fn neighbors() -> Vec<Self> {
-        let mut result = Vec::with_capacity(80);
-        for dw in -1..2 {
-            for dz in -1..2 {
-                for dy in -1..2 {
-                    for dx in -1..2 {
-                        if dx != 0 || dy != 0 || dz != 0 || dw != 0 {
-                            result.push(Coord4(dx, dy, dz, dw));
-                        }
-                    }
-                }
-            }
-        }
-        result
-    }
-}
-
-impl Add for Coord4 {
-    type Output = Coord4;
-    fn add(self, other: Coord4) -> Coord4 {
-        Coord4(self.0 + other.0, self.1 + other.1, self.2 + other.2, self.3 + other.3)
-    }
-}
-
-type State<C> = HashSet<C>;
-
-fn parse<C: Coord>(input: &str, new_coord: impl Fn(i64, i64) -> C) -> State<C> {
-    let new_coord_ref = &new_coord;
+fn parse(input: &str) -> State {
     input
         .lines()
         .enumerate()
@@ -72,18 +44,20 @@ fn parse<C: Coord>(input: &str, new_coord: impl Fn(i64, i64) -> C) -> State<C> {
                 .bytes()
                 .enumerate()
                 .filter(|&(_x, c)| c == b'#')
-                .map(move |(x, _c)| new_coord_ref(x as i64, y as i64))
+                .map(move |(x, _c)| Coord::new(
+                        x as <Coord as SimdVector>::Element,
+                        y as <Coord as SimdVector>::Element,
+                        0,
+                        0))
         })
         .collect()
 }
 
-fn step<C: Coord>(state: State<C>) -> State<C> {
-    let neighbors = C::neighbors();
-
+fn step(state: State, neighbors: &Vec<Coord>) -> State {
     let mut neigh_count = HashMap::with_capacity(2 * state.len());
     for p in state.iter().copied() {
         neigh_count.entry(p).or_default();
-        for &d in neighbors.iter() {
+        for &d in neighbors {
             *neigh_count.entry(p + d).or_default() += 1;
         }
     }
@@ -102,9 +76,10 @@ fn step<C: Coord>(state: State<C>) -> State<C> {
 }
 
 fn part1(input: &str) -> usize {
-    let mut state = parse(input, |x, y| Coord3(x, y, 0));
+    let mut state = parse(input);
+    let neighbors = neighbors_3d();
     for _ in 0..6 {
-        state = step(state);
+        state = step(state, &neighbors);
     }
     state.len()
 }
@@ -116,9 +91,10 @@ fn test_part1() {
 }
 
 fn part2(input: &str) -> usize {
-    let mut state = parse(input, |x, y| Coord4(x, y, 0, 0));
+    let mut state = parse(input);
+    let neighbors = neighbors_4d();
     for _ in 0..6 {
-        state = step(state);
+        state = step(state, &neighbors);
     }
     state.len()
 }
