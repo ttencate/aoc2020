f665ec07066ca6a16c8c4fdba74f3bb9e0879098
Thomas ten Cate <ttencate@gmail.com>
2020-12-24 10:19:34 +0100

    Solve 24b

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -1,6 +1,8 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-fn final_tile(line: &str) -> (i32, i32) {
+type Coord = (i32, i32);
+
+fn final_tile(line: &str) -> Coord {
     let (mut x, mut y) = (0, 0);
     let mut bytes = line.bytes();
     while let Some(c) = bytes.next() {
@@ -25,7 +27,7 @@ fn final_tile(line: &str) -> (i32, i32) {
     (x, y)
 }
 
-fn part1(input: &str) -> usize {
+fn initial_black(input: &str) -> HashSet<Coord> {
     input
         .lines()
         .map(|line| final_tile(line))
@@ -37,7 +39,10 @@ fn part1(input: &str) -> usize {
             }
             black
         })
-        .len()
+}
+
+fn part1(input: &str) -> usize {
+    initial_black(input).len()
 }
 
 #[test]
@@ -46,14 +51,35 @@ fn test_part1() {
     assert_eq!(part1(&aoc::input()), 322);
 }
 
-fn part2(_input: &str) -> String {
-    "TODO".to_string()
+fn part2(input: &str) -> usize {
+    let mut black = initial_black(input);
+    for _ in 0..100 {
+        let mut neigh_count = HashMap::<Coord, usize>::with_capacity(2 * black.len());
+        for &(x, y) in &black {
+            neigh_count.entry((x, y)).or_default();
+            for (dx, dy) in [(1, -1), (1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1)].iter() {
+                *neigh_count.entry((x + dx, y + dy)).or_default() += 1;
+            }
+        }
+        black = neigh_count
+            .iter()
+            .filter_map(|(coord, &count)| {
+                let is_black = if black.contains(coord) {
+                    count == 1 || count == 2
+                } else {
+                    count == 2
+                };
+                if is_black { Some(*coord) } else { None }
+            })
+            .collect();
+    }
+    black.len()
 }
 
 #[test]
 fn test_part2() {
-    // assert_eq!(part2(&aoc::example(0)), );
-    // assert_eq!(part2(&aoc::input()), );
+    assert_eq!(part2(&aoc::example(0)), 2208);
+    assert_eq!(part2(&aoc::input()), 3831);
 }
 
 fn main() {
